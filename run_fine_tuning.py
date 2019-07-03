11e20a47b9de25b169aec4506c809927ed33ec33
sjwilczynski <sjwilczynski@gmail.com>
2019-07-03 19:44:17 +0200

    adjusted fine tuning

--- run_fine_tuning.py
+++ run_fine_tuning.py
@@ -10,6 +10,8 @@ os.makedirs(target_directory, exist_ok=True)
 
 scoring = config['scoring']
 cv = int(config['cv'])
+cv_out = int(config['cv_out'])
+cv_in = int(config['cv_in'])
 n_iter = int(config['n_iter'])
 n_jobs = int(config['n_jobs'])
 notebook = config['notebook']
@@ -19,5 +21,5 @@ params_str = '_{}_{}_{}'.format(scoring, cv, n_iter)
 pm.execute_notebook(
    notebook + '.ipynb',
    target_directory + notebook + params_str +'.ipynb',
-   parameters = dict(scoring=scoring, cv=cv, n_iter=n_iter, n_jobs=n_jobs)
+   parameters = dict(scoring=scoring, cv=cv, cv_out=cv_out, cv_in=cv_in, n_iter=n_iter, n_jobs=n_jobs)
 )
